03aa13ed433f9e5c86d4bf32f215f917fef52862
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2019-01-07 15:52:11 +0600

    Navigation Drawer Menu Implement Succesfully

--- app/src/main/java/com/remotearthsolutions/expensetracker/activities/LoginActivity.java
+++ app/src/main/java/com/remotearthsolutions/expensetracker/activities/LoginActivity.java
@@ -46,7 +46,7 @@ public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         // google button
         signInButton = findViewById(R.id.googlebutton);
