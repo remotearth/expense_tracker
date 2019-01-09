95f6f08f3fb92ce7d4bc5ad95c827870cbf8ddf8
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2019-01-09 16:52:26 +0600

    Design improved

--- app/src/main/java/com/remotearthsolutions/expensetracker/activities/MainActivity.java
+++ app/src/main/java/com/remotearthsolutions/expensetracker/activities/MainActivity.java
@@ -70,13 +70,17 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     // method for pie chart operation
     private void drawPie() {
 
-
         AnimatedPieView mAnimatedPieView = findViewById(R.id.animatedpie);
         AnimatedPieViewConfig config = new AnimatedPieViewConfig();
         config.startAngle(-90)// Starting angle offset
-                .addData(new SimplePieInfo(30, Color.parseColor("#00aaee"), "Medical"))
-                .addData(new SimplePieInfo(18.0f, Color.parseColor("#000000"), "Health"))
+                .addData(new SimplePieInfo(17.2f, Color.parseColor("#00aaee"), "Food"))
+                .addData(new SimplePieInfo(18.0f, Color.parseColor("#000000"), "Gift"))
+                .addData(new SimplePieInfo(11.0f, Color.parseColor("#FF008577"), "Bills"))
+                .addData(new SimplePieInfo(15.0f, Color.parseColor("#D81B60"), "Taxi"))
                 .canTouch(true)
+                .drawText(true)
+                .autoSize(true)
+                .textSize(50)
                 .duration(2000);
 
         mAnimatedPieView.applyConfig(config);
