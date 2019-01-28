0923b0938bd1bc7e08d63590178a95914e823be7
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2019-01-28 18:44:33 +0600

    pass value from one acriviy to presenter class

--- app/src/main/java/com/remotearthsolutions/expensetracker/activities/MainActivity.java
+++ app/src/main/java/com/remotearthsolutions/expensetracker/activities/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.view.MenuItem;
+import android.widget.Toast;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -59,6 +60,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @Override
     public void openLoginScreen() {
+
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
         finish();
@@ -113,6 +115,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     }
 
     public void openAddExpenseScreen(Category category) {
+
         ExpenseFragment expenseFragment = new ExpenseFragment();
         Parcelable wrappedCategory = Parcels.wrap(category);
         Bundle bundle = new Bundle();
