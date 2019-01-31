afc691b3f135c2f61d0b9e1980d3b1adba874050
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2019-01-31 19:16:48 +0600

    Date picker by button click & period setting preference added

--- app/src/main/java/com/remotearthsolutions/expensetracker/fragments/HomeFragment.java
+++ app/src/main/java/com/remotearthsolutions/expensetracker/fragments/HomeFragment.java
@@ -1,13 +1,12 @@
 package com.remotearthsolutions.expensetracker.fragments;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
@@ -31,13 +30,16 @@ import com.remotearthsolutions.expensetracker.utils.DateTimeUtils;
 import com.remotearthsolutions.expensetracker.viewmodels.CategoryFragmentViewModel;
 import com.remotearthsolutions.expensetracker.viewmodels.HomeFragmentViewModel;
 
+import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 
-public class HomeFragment extends Fragment implements ChartManagerImpl.ChartView, HomeFragmentContract.View {
+public class HomeFragment extends Fragment implements ChartManagerImpl.ChartView, HomeFragmentContract.View, View.OnClickListener {
 
     private CategoryListAdapter adapter;
     private AnimatedPieView mAnimatedPieView;
@@ -45,6 +47,8 @@ public class HomeFragment extends Fragment implements ChartManagerImpl.ChartView
     private HomeFragmentViewModel viewModel;
     private ImageView addCategory,nextDate,previousDate;
     private TextView showdate;
+    private Button dailyButton,weeklyButton,monthlyButton,yearlyButton;
+    private int cDay, cMonth, cYear;
 
 
     @Nullable
@@ -56,6 +60,18 @@ public class HomeFragment extends Fragment implements ChartManagerImpl.ChartView
         showdate = view.findViewById(R.id.showdateinhome);
         nextDate = view.findViewById(R.id.nextdatebutton);
         previousDate = view.findViewById(R.id.previousdatebutton);
+        dailyButton = view.findViewById(R.id.daily);
+        weeklyButton = view.findViewById(R.id.weekly);
+        monthlyButton= view.findViewById(R.id.monthly);
+        yearlyButton = view.findViewById(R.id.yearly);
+
+        addCategory.setOnClickListener(this);
+        nextDate.setOnClickListener(this);
+        previousDate.setOnClickListener(this);
+        dailyButton.setOnClickListener(this);
+        weeklyButton.setOnClickListener(this);
+        monthlyButton.setOnClickListener(this);
+        yearlyButton.setOnClickListener(this);
 
 
         mAnimatedPieView = view.findViewById(R.id.animatedpie);
@@ -72,51 +88,7 @@ public class HomeFragment extends Fragment implements ChartManagerImpl.ChartView
         BottomNavigationView navigation = view.findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        addCategory.setOnClickListener(v -> {
-
-            FragmentManager fm = getChildFragmentManager();
-            final AddCategoryDialogFragment categoryDialogFragment = AddCategoryDialogFragment.newInstance("Add Category");
-            categoryDialogFragment.setCallback(new AddCategoryDialogFragment.Callback() {
-                @Override
-                public void onCategoryAdded(CategoryModel categoryModel) {
-
-                    categoryDialogFragment.dismiss();
-
-                }
-            });
-            categoryDialogFragment.show(fm, AddCategoryDialogFragment.class.getName());
-
-        });
-
         showdate.setText(DateTimeUtils.getCurrentDate(DateTimeUtils.dd_MM_yyyy));
-
-
-        nextDate.setOnClickListener(v -> {
-
-            SimpleDateFormat sdf = new SimpleDateFormat(DateTimeUtils.dd_MM_yyyy);
-            try {
-
-                Date getCurrentDate = sdf.parse(DateTimeUtils.getCurrentDate(DateTimeUtils.dd_MM_yyyy));
-                Date getNextDate = sdf.parse(DateTimeUtils.getDate(DateTimeUtils.dd_MM_yyyy,+1));
-
-                if (getNextDate.compareTo(getCurrentDate) > 0)
-                {
-                    showdate.setText(DateTimeUtils.getCurrentDate(DateTimeUtils.dd_MM_yyyy));
-                }
-
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-
-        });
-
-        previousDate.setOnClickListener(v -> {
-
-            String previousdate = DateTimeUtils.getDate(DateTimeUtils.dd_MM_yyyy, -1);
-            showdate.setText(previousdate);
-
-        });
-
         return view;
     }
 
@@ -126,24 +98,20 @@ public class HomeFragment extends Fragment implements ChartManagerImpl.ChartView
     }
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    Toast.makeText(getActivity(), "Clicked On Bottom menu 1", Toast.LENGTH_LONG).show();
-                    return true;
-                case R.id.navigation_dashboard:
-                    Toast.makeText(getActivity(), "Clicked On Bottom Menu 2", Toast.LENGTH_LONG).show();
-                    return true;
-                case R.id.navigation_notifications:
-                    Toast.makeText(getActivity(), "Clicked On Bottom Menu 3", Toast.LENGTH_LONG).show();
-                    return true;
-            }
-            return false;
-        }
-    };
+            = item -> {
+                switch (item.getItemId()) {
+                    case R.id.navigation_home:
+                        Toast.makeText(getActivity(), "Clicked On Bottom menu 1", Toast.LENGTH_LONG).show();
+                        return true;
+                    case R.id.navigation_dashboard:
+                        Toast.makeText(getActivity(), "Clicked On Bottom Menu 2", Toast.LENGTH_LONG).show();
+                        return true;
+                    case R.id.navigation_notifications:
+                        Toast.makeText(getActivity(), "Clicked On Bottom Menu 3", Toast.LENGTH_LONG).show();
+                        return true;
+                }
+                return false;
+            };
 
     @Override
     public void showCategories(List<CategoryModel> categories) {
@@ -164,5 +132,118 @@ public class HomeFragment extends Fragment implements ChartManagerImpl.ChartView
         ChartManager chartManager = new ChartManagerImpl();
         chartManager.initPierChart();
         chartManager.loadExpensePieChart(this, listOfCategoryWithAmount);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.addcategoryinhome) {
+
+            FragmentManager fm = getChildFragmentManager();
+            final AddCategoryDialogFragment categoryDialogFragment = AddCategoryDialogFragment.newInstance("Add Category");
+            categoryDialogFragment.setCallback(new AddCategoryDialogFragment.Callback() {
+                @Override
+                public void onCategoryAdded(CategoryModel categoryModel) {
+
+                    categoryDialogFragment.dismiss();
+
+                }
+            });
+            categoryDialogFragment.show(fm, AddCategoryDialogFragment.class.getName());
+
+        }
+
+        else if (v.getId() == R.id.nextdatebutton) {
+
+            SimpleDateFormat sdf = new SimpleDateFormat(DateTimeUtils.dd_MM_yyyy);
+            try {
+
+                Date getCurrentDate = sdf.parse(DateTimeUtils.getCurrentDate(DateTimeUtils.dd_MM_yyyy));
+                Date getNextDate = sdf.parse(DateTimeUtils.getDate(DateTimeUtils.dd_MM_yyyy,+1));
+
+                if (getNextDate.compareTo(getCurrentDate) > 0)
+                {
+                    showdate.setText(DateTimeUtils.getCurrentDate(DateTimeUtils.dd_MM_yyyy));
+                }
+
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+        else if (v.getId() == R.id.previousdatebutton) {
+
+            String previousdate = DateTimeUtils.getDate(DateTimeUtils.dd_MM_yyyy, -1);
+            showdate.setText(previousdate);
+        }
+
+        else if (v.getId() == R.id.daily)
+        {
+
+            DatePicker datePicker = new DatePicker(getActivity());
+            cDay = datePicker.getDayOfMonth();
+            cMonth = datePicker.getMonth()+1;
+            cYear = datePicker.getYear();
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                    showdate.setText(dayOfMonth+"/"+month+"/"+year);
+
+                }
+            }, cYear, cMonth, cDay);
+            datePickerDialog.show();
+        }
+
+        else if (v.getId() == R.id.weekly)
+        {
+
+
+        }
+
+        else if (v.getId() == R.id.monthly)
+        {
+            DatePicker datePicker = new DatePicker(getActivity());
+            cDay = datePicker.getDayOfMonth();
+            cMonth = datePicker.getMonth()+1;
+            cYear = datePicker.getYear();
+
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                    showdate.setText(month+"/"+year);
+
+                }
+            }, cYear, cMonth, cDay);
+            datePickerDialog.show();
+
+        }
+
+        else if (v.getId() == R.id.yearly)
+        {
+            DatePicker datePicker = new DatePicker(getActivity());
+            cDay = datePicker.getDayOfMonth();
+            cMonth = datePicker.getMonth()+1;
+            cYear = datePicker.getYear();
+
+            DatePickerDialog datePickerDialog = new DatePickerDialog(getActivity(), new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+
+                    showdate.setText(year);
+
+                }
+            }, cYear, cMonth, cDay);
+            datePickerDialog.show();
+
+        }
+
+
+
     }
 }
