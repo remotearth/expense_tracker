fe05e689b1bc622a17bfec4429a25c648f7b6921
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2019-02-10 11:58:34 +0600

    implement app closing on pressing back button twice succesfully

--- app/src/main/java/com/remotearthsolutions/expensetracker/activities/MainActivity.java
+++ app/src/main/java/com/remotearthsolutions/expensetracker/activities/MainActivity.java
@@ -88,16 +88,20 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         if (drawer.isDrawerOpen(GravityCompat.START)) {
             drawer.closeDrawer(GravityCompat.START);
-
         }
-        else {
+        else if (getSupportFragmentManager().getBackStackEntryCount() == 1)
+        {
 
+            super.onBackPressed();
+        }
+        else
+        {
             long t = System.currentTimeMillis();
             if (t - backPressedTime > 2000)
             {
                 backPressedTime = t;
-                Toast.makeText(this, "Press back again to logout", Toast.LENGTH_SHORT).show();
-                super.onBackPressed();
+                Toast.makeText(this, "Press once again to close app", Toast.LENGTH_SHORT).show();
+
             }
             else
             {
@@ -200,8 +204,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         expenseFragment.setArguments(bundle);
         FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
         fragmentTransaction.setCustomAnimations(R.anim.slide_in_up, 0, 0, R.anim.slide_out_down);
-        fragmentTransaction.addToBackStack(null);
         fragmentTransaction.add(R.id.framelayout, expenseFragment, ExpenseFragment.class.getName());
+        fragmentTransaction.addToBackStack("home");
         fragmentTransaction.commit();
     }
 }
