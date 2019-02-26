81c6b88c44b9401b27876ed538535a165efc07df
MD. Aminul Haque Aome <aminul15-5281@diu.edu.bd>
2019-02-26 14:13:37 +0600

    Implemented Attaching csv file to mail

--- app/src/main/java/com/remotearthsolutions/expensetracker/viewmodels/ExpenseViewModel.java
+++ app/src/main/java/com/remotearthsolutions/expensetracker/viewmodels/ExpenseViewModel.java
@@ -1,12 +1,9 @@
 package com.remotearthsolutions.expensetracker.viewmodels;
 
-import android.app.Activity;
 import androidx.lifecycle.ViewModel;
 import com.remotearthsolutions.expensetracker.contracts.ExpenseFragmentContract;
 import com.remotearthsolutions.expensetracker.databaseutils.daos.ExpenseDao;
 import com.remotearthsolutions.expensetracker.databaseutils.models.dtos.CategoryExpense;
-import com.remotearthsolutions.expensetracker.services.FileProcessingService;
-import com.remotearthsolutions.expensetracker.services.FileProcessingServiceImp;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
@@ -19,13 +16,11 @@ public class ExpenseViewModel extends ViewModel {
     private ExpenseFragmentContract.ExpenseView view;
     private ExpenseDao expenseDao;
     private CompositeDisposable disposable = new CompositeDisposable();
-    private FileProcessingService fileProcessingService;
+
 
     public ExpenseViewModel(ExpenseFragmentContract.ExpenseView view, ExpenseDao expenseDao) {
         this.view = view;
         this.expenseDao = expenseDao;
-
-        fileProcessingService = new FileProcessingServiceImp();
     }
 
     public void loadFilterExpense(long startTime, long endTime) {
@@ -46,23 +41,6 @@ public class ExpenseViewModel extends ViewModel {
                     }
 
                     view.loadFilterExpense(expenseList);
-
-                }));
-    }
-
-
-    public void saveExpenseToCSV(Activity activity, long startTime, long endTime) {
-
-        disposable.add(expenseDao.getAllFilterExpense(startTime, endTime)
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(listOfFilterExpense -> {
-
-                    if (listOfFilterExpense != null) {
-                        for (int i = 0; i < listOfFilterExpense.size(); i++) {
-                            fileProcessingService.writeOnCsvFile(activity, listOfFilterExpense.get(i));
-                        }
-                    }
 
                 }));
     }
