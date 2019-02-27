3de4d4b021381d7013d24e147ea645e9299ab6ad
Tahmid Mozaffar <tahmid1989@gmail.com>
2019-02-28 00:55:55 +0600

    refacted dashboard item list adapter

--- app/src/main/java/com/remotearthsolutions/expensetracker/adapters/DashboardAdapter.java
+++ app/src/main/java/com/remotearthsolutions/expensetracker/adapters/DashboardAdapter.java
@@ -19,7 +19,7 @@ public class DashboardAdapter extends ArrayAdapter<DashboardModel> {
     private Context context;
     private ArrayList<DashboardModel> dashboardItem;
 
-    public DashboardAdapter(Context context, ArrayList<DashboardModel> dashboardItem)  {
+    public DashboardAdapter(Context context, ArrayList<DashboardModel> dashboardItem) {
         super(context, R.layout.custom_dashboard, dashboardItem);
         this.context = context;
         this.dashboardItem = dashboardItem;
@@ -29,12 +29,19 @@ public class DashboardAdapter extends ArrayAdapter<DashboardModel> {
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
 
-        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        convertView = inflater.inflate(R.layout.custom_dashboard,parent,false);
-        ImageView imageView = convertView.findViewById(R.id.dashboardimageView);
-        TextView textView = convertView.findViewById(R.id.dashboardtextView);
-        imageView.setImageResource(dashboardItem.get(position).getDashboardImage());
-        textView.setText(dashboardItem.get(position).getDashboardName());
-        return convertView;
+        View view = convertView;
+
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.custom_dashboard, parent, false);
+        }
+
+        DashboardModel model = dashboardItem.get(position);
+        ImageView imageView = view.findViewById(R.id.dashboardimageView);
+        TextView textView = view.findViewById(R.id.dashboardtextView);
+
+        imageView.setImageResource(model.getDashboardImage());
+        textView.setText(model.getDashboardName());
+
+        return view;
     }
 }
