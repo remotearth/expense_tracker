0cecb0b06d5bda5116db6e40f0017d14b49f76b1
Nayeem Shiddiki Abir <abircoxsbazar@gmail.com>
2019-03-05 13:34:42 +0600

    Privacy Policies Added

--- app/src/main/java/com/remotearthsolutions/expensetracker/utils/Constants.java
+++ app/src/main/java/com/remotearthsolutions/expensetracker/utils/Constants.java
@@ -6,7 +6,7 @@ public interface Constants {
     String KEY_SELECTED_ACCOUNT_ID = "KEY_SELECTED_ACCOUNT_ID";
     String KEY_USER = "KEY_USER";
     String KEY_URL = "KEY";
-    String URL_PRIVACY_POLICY = "http://www.facebook.com";
+    String URL_PRIVACY_POLICY = "file:///android_asset/privacypolicy/PrivacyPolicy.html";
     String URL_LICENSES = "http://www.google.com";
     String URL_PRIVACY_POLICY_TAG = "PRIVACYTAG";
     String URL_lICENSES_TAG = "LICENSESTAG";
