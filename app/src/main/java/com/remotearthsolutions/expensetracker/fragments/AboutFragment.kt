ce64488bb3053a14251dffc9d170626a95551dcf
Tahmid Mozaffar <tahmid1989@gmail.com>
2020-05-21 04:36:23 -0500

    Added backdoor to make the app premium

--- app/src/main/java/com/remotearthsolutions/expensetracker/fragments/AboutFragment.kt
+++ app/src/main/java/com/remotearthsolutions/expensetracker/fragments/AboutFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.remotearthsolutions.expensetracker.BuildConfig
 import com.remotearthsolutions.expensetracker.R
+import com.remotearthsolutions.expensetracker.activities.ApplicationObject
 import kotlinx.android.synthetic.main.fragment_about.view.*
 
 class AboutFragment : BaseFragment() {
@@ -26,5 +27,13 @@ class AboutFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         registerBackButton()
+        var versionNumberClickCount = 0
+        view.versionno.setOnClickListener {
+            versionNumberClickCount++
+            if (versionNumberClickCount == 10) {
+                versionNumberClickCount = 0
+                (activity?.application as ApplicationObject).isPremium = true
+            }
+        }
     }
 }
