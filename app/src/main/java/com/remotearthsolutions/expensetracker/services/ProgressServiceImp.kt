61b95e1a24c118c5d204e8c7f494cde2fbcffc97
Tahmid Mozaffar <tahmid1989@gmail.com>
2020-05-27 02:44:02 -0500

    Added new progress bar design

--- app/src/main/java/com/remotearthsolutions/expensetracker/services/ProgressServiceImp.kt
+++ app/src/main/java/com/remotearthsolutions/expensetracker/services/ProgressServiceImp.kt
@@ -1,21 +1,48 @@
 package com.remotearthsolutions.expensetracker.services
 
-import android.app.ProgressDialog
+import android.app.Dialog
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.TextView
+import com.remotearthsolutions.expensetracker.R
+import com.wang.avi.AVLoadingIndicatorView
 
 class ProgressServiceImp(private val mContext: Context) : ProgressService {
-    private var mProgressDialog: ProgressDialog? = null
+    private var mProgressDialog: Dialog? = null
     override fun showProgressBar(message: String?) {
-        if (mProgressDialog == null) {
-            mProgressDialog = ProgressDialog(mContext)
-            mProgressDialog!!.setCancelable(false)
-            mProgressDialog!!.setMessage(message)
-            mProgressDialog!!.setProgressStyle(ProgressDialog.STYLE_SPINNER)
+
+        hideProgressBar()
+        mProgressDialog = Dialog(mContext, R.style.ProgressViewTheme)
+        val view: View =
+            (mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
+                .inflate(R.layout.progress_indicator, null)
+        val progressView: AVLoadingIndicatorView = view.findViewById(R.id.progressView)
+        val progressMessageTv = view.findViewById<TextView>(R.id.progressMessageTv)
+
+        progressMessageTv.text = message
+        progressView.show()
+
+        mProgressDialog?.setContentView(view)
+        mProgressDialog?.setCancelable(false)
+
+        try {
+            mProgressDialog?.show()
+        } catch (e: Exception) {
+            e.printStackTrace()
         }
-        mProgressDialog!!.show()
     }
 
     override fun hideProgressBar() {
+        try {
+            if (mProgressDialog != null) {
+                mProgressDialog?.dismiss()
+                mProgressDialog = null
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
         if (mProgressDialog != null && mProgressDialog!!.isShowing) {
             mProgressDialog!!.dismiss()
         }
