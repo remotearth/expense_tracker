38ab0ae75a92ac86622c107f235d1c794b5df28b
Tahmid Mozaffar <tahmid1989@gmail.com>
2020-06-14 03:58:37 -0500

    Option to share User id from about page

--- app/src/main/java/com/remotearthsolutions/expensetracker/fragments/AboutFragment.kt
+++ app/src/main/java/com/remotearthsolutions/expensetracker/fragments/AboutFragment.kt
@@ -1,6 +1,9 @@
 package com.remotearthsolutions.expensetracker.fragments
 
 import android.annotation.SuppressLint
+import android.content.ClipData
+import android.content.ClipboardManager
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +11,9 @@ import android.view.ViewGroup
 import com.remotearthsolutions.expensetracker.BuildConfig
 import com.remotearthsolutions.expensetracker.R
 import com.remotearthsolutions.expensetracker.activities.ApplicationObject
+import com.remotearthsolutions.expensetracker.utils.AlertDialogUtils
+import com.remotearthsolutions.expensetracker.utils.Constants
+import com.remotearthsolutions.expensetracker.utils.SharedPreferenceUtils
 import kotlinx.android.synthetic.main.fragment_about.view.*
 
 class AboutFragment : BaseFragment() {
@@ -27,12 +33,30 @@ class AboutFragment : BaseFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         registerBackButton()
-        var versionNumberClickCount = 0
+        var clickOnVersionNumberCount = 0
         view.versionno.setOnClickListener {
-            versionNumberClickCount++
-            if (versionNumberClickCount == 10) {
-                versionNumberClickCount = 0
+            clickOnVersionNumberCount++
+            if (clickOnVersionNumberCount == 10) {
+                clickOnVersionNumberCount = 0
                 (activity?.application as ApplicationObject).isPremium = true
+            }
+        }
+
+        var clickOnLogoCount = 0
+        view.logo.setOnClickListener {
+            clickOnLogoCount++
+            if (clickOnLogoCount == 5) {
+                clickOnLogoCount = 0
+                val clipboard =
+                    requireContext().getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                val userId = SharedPreferenceUtils.getInstance(requireContext())
+                    ?.getString(Constants.KEY_USER, "")!!
+                clipboard.setPrimaryClip(ClipData.newPlainText("userid", "UserID: $userId"))
+                AlertDialogUtils.show(
+                    requireContext(), null,
+                    "Your UserID is copied to clipboard. Please paste it on your email client and send to remotearth.solutions@gmail.com",
+                    "Ok", null, null, null
+                )
             }
         }
     }
