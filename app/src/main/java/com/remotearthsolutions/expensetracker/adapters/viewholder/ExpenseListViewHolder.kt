a3a82b420db98424498ca4f19af8ce7f7f537565
Tahmid Mozaffar <tahmid1989@gmail.com>
2021-02-11 20:18:56 -0600

    expense note is shown in transaction tab in the list

--- app/src/main/java/com/remotearthsolutions/expensetracker/adapters/viewholder/ExpenseListViewHolder.kt
+++ app/src/main/java/com/remotearthsolutions/expensetracker/adapters/viewholder/ExpenseListViewHolder.kt
@@ -16,6 +16,7 @@ class ExpenseListViewHolder(
     private val listener: ExpenseListAdapter.OnItemClickListener
 ) : RecyclerView.ViewHolder(itemView) {
     private val categoryTextView: TextView = itemView.findViewById(R.id.categoryNameTv)
+    private val notesTextView: TextView = itemView.findViewById(R.id.notesTv)
     private val amountTextView: TextView = itemView.findViewById(R.id.amountTv)
     private val accountNameTv: TextView = itemView.findViewById(R.id.accountNameTv)
     private val categoryExpenseIcon: ImageView = itemView.findViewById(R.id.categoryIMG)
@@ -29,8 +30,18 @@ class ExpenseListViewHolder(
         currencySymbol: String,
         isDeleteModeOn: Boolean
     ) {
+        notesTextView.visibility = View.GONE
+
         this.expense = expense
         categoryTextView.text = expense.categoryName.toString()
+
+        expense.note?.let {
+            if (it.isNotEmpty()) {
+                notesTextView.text = it.trim()
+                notesTextView.visibility = View.VISIBLE
+            }
+        }
+
         amountTextView.text = "$currencySymbol ${formatDecimalValues(expense.totalAmount)}"
         categoryExpenseIcon.setImageResource(getIconId(expense.categoryIcon!!))
         accountNameTv.text = expense.accountName
